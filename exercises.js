f789e319f135852b8976ca3053d89e931fc5cb3e
cryosx <cyruswu.email@gmail.com>
2018-02-13 20:46:48 -1000

    ex14

--- exercises.js
+++ exercises.js
@@ -264,7 +264,15 @@ Note, please use two for loops to complete this problem
 
 var guide = [["Honolulu", "Waikiki"], ["Tokyo", "Tsukiji Fish Market"], ["Cairo", "Pyramids"],["Rome", "Colosseum"]];
 
+function sightseeing(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		for (var j = 0; j < arr[i].length; j++) {
+			console.log(arr[i][j]);
+		}
+	}
+}
 
+sightseeing(guide);
 
 /* 15) Back to School
 Declare a variable named `currentCohort` and set it's value to be this [array found here](https://gist.github.com/sgnl/e40879b2249e06ca7811).
